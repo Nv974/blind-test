71c137417473eb69d7e70573c4e4935fe0685b45
Nicolas Vitry <74904811+Nv974@users.noreply.github.com>
2021-11-24 10:49:42 +0400

    add react hook form and text input : artist

--- src/screens/Plays.js
+++ src/screens/Plays.js
@@ -1,19 +1,88 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    TouchableOpacity,
+    TextInput,
+} from 'react-native';
+
+import { useForm, Controller } from 'react-hook-form';
 
 const Play = () => {
     const [isStarted, setisStarted] = useState(false);
+
+    // fonctionnalités react hook form
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
     return (
         <View style={styles.container}>
             <Text> Play Component </Text>
             {!isStarted && <Button title='Play' onPress={() => setisStarted(true)} />}
+            {isStarted && (
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        marginBottom: 20,
+                    }}
+                >
+                    <Controller
+                        control={control}
+                        render={({ value, field: { onChange } }) => (
+                            <TextInput
+                                style={styles.input}
+                                value={value}
+                                onChangeText={value => onChange(value)}
+                                placeholder="Tapez le nom de l'artiste"
+                                multiline={false}
+                                autoFocus={true}
+                                autoCorrect={false}
+                                //onSubmitEditing={handleSubmit()}
+                            />
+                        )}
+                        name='artist'
+                    />
+
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        style={styles.submit}
+                        //onPress={handleSubmit())}
+                    >
+                        <Text style={{ color: 'white' }}> OK </Text>
+                    </TouchableOpacity>
+                </View>
+            )}
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {},
+    input: {
+        padding: 10,
+        fontSize: 17,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5,
+        width: 200,
+    },
+
+    submit: {
+        backgroundColor: 'black',
+        width: 60,
+        padding: 5,
+        borderTopRightRadius: 5,
+        borderBottomRightRadius: 5,
+
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
 export default Play;
