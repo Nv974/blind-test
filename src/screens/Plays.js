f0de963a2bd33d3842662099c44828c9d440c4de
Nicolas Vitry <74904811+Nv974@users.noreply.github.com>
2021-11-24 11:57:24 +0400

    add textinput controllers artist and title

--- src/screens/Plays.js
+++ src/screens/Plays.js
@@ -12,49 +12,96 @@ import { useForm, Controller } from 'react-hook-form';
 
 const Play = () => {
     const [isStarted, setisStarted] = useState(false);
+    const [showTitleInput, setShowTitleInput] = useState(false);
 
     // fonctionnalités react hook form
     const {
         control,
         handleSubmit,
+        reset,
         formState: { errors },
     } = useForm();
 
+    const onSubmitArtistNameHandler = data => {
+        console.log(data);
+        setShowTitleInput(true);
+        reset();
+    };
+
+    const onSubmitTitleHandler = data => {
+        console.log(data);
+        setShowTitleInput(false);
+        reset();
+    };
+
     return (
         <View style={styles.container}>
-            <Text> Play Component </Text>
             {!isStarted && <Button title='Play' onPress={() => setisStarted(true)} />}
             {isStarted && (
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        marginBottom: 20,
-                    }}
-                >
-                    <Controller
-                        control={control}
-                        render={({ value, field: { onChange } }) => (
-                            <TextInput
-                                style={styles.input}
-                                value={value}
-                                onChangeText={value => onChange(value)}
-                                placeholder="Tapez le nom de l'artiste"
-                                multiline={false}
-                                autoFocus={true}
-                                autoCorrect={false}
-                                //onSubmitEditing={handleSubmit()}
+                <View>
+                    <Text>
+                        {showTitleInput ? 'Titre du morceau' : "Nom de l'artiste"}
+                    </Text>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            marginBottom: 20,
+                        }}
+                    >
+                        {!showTitleInput ? (
+                            <Controller
+                                control={control}
+                                render={({ value, field: { onChange } }) => (
+                                    <TextInput
+                                        style={styles.input}
+                                        value={value}
+                                        onChangeText={value => onChange(value)}
+                                        placeholder="Tapez le nom de l'artiste"
+                                        multiline={false}
+                                        autoFocus={true}
+                                        autoCorrect={false}
+                                        onSubmitEditing={handleSubmit(
+                                            onSubmitArtistNameHandler,
+                                        )}
+                                    />
+                                )}
+                                rules={{ required: true }}
+                                name='artist'
+                            />
+                        ) : (
+                            <Controller
+                                control={control}
+                                render={({ value, field: { onChange } }) => (
+                                    <TextInput
+                                        style={styles.input}
+                                        value={value}
+                                        onChangeText={value => onChange(value)}
+                                        placeholder='Tapez le titre du morceau'
+                                        multiline={false}
+                                        autoFocus={true}
+                                        autoCorrect={false}
+                                        onSubmitEditing={handleSubmit(
+                                            onSubmitTitleHandler,
+                                        )}
+                                    />
+                                )}
+                                rules={{ required: true }}
+                                name='title'
                             />
                         )}
-                        name='artist'
-                    />
 
-                    <TouchableOpacity
-                        activeOpacity={0.8}
-                        style={styles.submit}
-                        //onPress={handleSubmit())}
-                    >
-                        <Text style={{ color: 'white' }}> OK </Text>
-                    </TouchableOpacity>
+                        <TouchableOpacity
+                            activeOpacity={0.8}
+                            style={styles.submit}
+                            onPress={handleSubmit(
+                                !showTitleInput
+                                    ? onSubmitArtistNameHandler
+                                    : onSubmitTitleHandler,
+                            )}
+                        >
+                            <Text style={{ color: 'white' }}> OK </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             )}
         </View>
@@ -70,7 +117,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderTopLeftRadius: 5,
         borderBottomLeftRadius: 5,
-        width: 200,
+        width: 250,
     },
 
     submit: {
